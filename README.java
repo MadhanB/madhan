827ca4e4218faa6990e23abb316634b33e580edb
MadhanB <smartmadhan07@gmail.com>
2016-07-02 16:48:38 +0530

    finish

---
+++ README.java
@@ -0,0 +1,12 @@
+# madhan
+class student
+{
+public static void main(String[] args)
+{
+String student = "madhan";
+StringBuilder test = new StringBuilder(student);
+student = test.reverse().toString();
+System.out.println(student);
+}
+
+}
